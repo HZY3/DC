158f698ae0497480ec44aa8085c3dbc963af0fd6
ShubhamKannaujiya <78816945+ShubhamKannaujiya@users.noreply.github.com>
2023-04-27 08:10:52 +0530

    Update Server.py

--- EXP 2/Server.py
+++ EXP 2/Server.py
@@ -28,3 +28,38 @@ server.register_function(divide, 'divide')
 
 print("Starting server...")
 server.serve_forever()
+
+
+
+Remote Procedure Call (RPC) is a powerful technique for constructing distributed, client-server based applications. It is based on extending the conventional local procedure calling so that the called procedure need not exist in the same address space as the calling procedure. The two processes may be on the same system, or they may be on different systems with a network connecting them. 
+
+When making a Remote Procedure Call: 
+
+
+
+1. The calling environment is suspended, procedure parameters are transferred across the network to the environment where the procedure is to execute, and the procedure is executed there. 
+
+2. When the procedure finishes and produces its results, its results are transferred back to the calling environment, where execution resumes as if returning from a regular procedure call. 
+
+NOTE: RPC is especially well suited for client-server (e.g. query-response) interaction in which the flow of control alternates between the caller and callee. Conceptually, the client and server do not both execute at the same time. Instead, the thread of execution jumps from the caller to the callee and then back again. 
+
+Working of RPC 
+
+
+
+The following steps take place during a RPC : 
+
+A client invokes a client stub procedure, passing parameters in the usual way. The client stub resides within the client’s own address space. 
+The client stub marshalls(pack) the parameters into a message. Marshalling includes converting the representation of the parameters into a standard format, and copying each parameter into the message. 
+The client stub passes the message to the transport layer, which sends it to the remote server machine. 
+On the server, the transport layer passes the message to a server stub, which demarshalls(unpack) the parameters and calls the desired server routine using the regular procedure call mechanism. 
+When the server procedure completes, it returns to the server stub (e.g., via a normal procedure call return), which marshalls the return values into a message. The server stub then hands the message to the transport layer. 
+The transport layer sends the result message back to the client transport layer, which hands the message back to the client stub. 
+The client stub demarshalls the return parameters and execution returns to the caller.
+Key Considerations for Designing and Implementing RPC Systems are: 
+
+Security: Since RPC involves communication over the network, security is a major concern. Measures such as authentication, encryption, and authorization must be implemented to prevent unauthorized access and protect sensitive data.
+Scalability: As the number of clients and servers increases, the performance of the RPC system must not degrade. Load balancing techniques and efficient resource utilization are important for scalability.
+Fault tolerance: The RPC system should be resilient to network failures, server crashes, and other unexpected events. Measures such as redundancy, failover, and graceful degradation can help ensure fault tolerance.
+Standardization: There are several RPC frameworks and protocols available, and it is important to choose a standardized and widely accepted one to ensure interoperability and compatibility across different platforms and programming languages.
+Performance tuning: Fine-tuning the RPC system for optimal performance is important. This may involve optimizing the network protocol, minimizing the data transferred over the network, and reducing the latency and overhead associated with RPC calls.
