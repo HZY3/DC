f81e990234f18e8e125f620f7dbead77c18ee726
ShubhamKannaujiya <78816945+ShubhamKannaujiya@users.noreply.github.com>
2023-04-27 08:04:00 +0530

    Update Deadlock Management.py

--- EXP8/Deadlock Management.py
+++ EXP8/Deadlock Management.py
@@ -80,3 +80,34 @@ if safeSequenceFound:
     print ("Safe Sequcne : ", safeSequence)
 else:
     print ("deadlock detected")
+
+    ########################################################################################################################
+    Various approaches to detect the deadlock in the distributed system are as follows:
+
+1. Centralized Approach
+
+Only one resource is responsible for detecting deadlock in the centralized method, and it is simple and easy to use. Still, the disadvantages include excessive workload on a single node and single-point failure (i.e., the entire system is dependent on one node, and if that node fails, the entire system crashes), making the system less reliable.
+
+2. Hierarchical Approach
+
+In a distributed system, it is the integration of both centralized and distributed approaches to deadlock detection. In this strategy, a single node handles a set of selected nodes or clusters of nodes that are in charge of deadlock detection.
+
+3. Distributed Approach
+
+In the distributed technique, various nodes work to detect deadlocks. There is no single point of failure as the workload is equally spread among all nodes. It also helps to increase the speed of deadlock detection.
+   
+    1. Deadlock Prevention: As the name implies, this strategy ensures that deadlock can never happen because system designing is carried out in such a way. If any one of the deadlock-causing conditions is not met then deadlock can be prevented. Following are the three methods used for preventing deadlocks by making one of the deadlock conditions to be unsatisfied:
+
+Collective Requests: In this strategy, all the processes will declare the required resources for their execution beforehand and will be allowed to execute only if there is the availability of all the required resources. When the process ends up with processing then only resources will be released. Hence, the hold and wait condition of deadlock will be prevented.
+But the issue is initial resource requirements of a process before it starts are based on an assumption and not because they will be required. So, resources will be unnecessarily occupied by a process and prior allocation of resources also affects potential concurrency.
+Ordered Requests: In this strategy, ordering is imposed on the resources and thus, process requests for resources in increasing order. Hence, the circular wait condition of deadlock can be prevented. 
+An ordering strictly indicates that a process never asks for a low resource while holding a high one.
+There are two more ways of dealing with global timing and transactions in distributed systems, both of which are based on the principle of assigning a global timestamp to each transaction as soon as it begins.
+    2. Deadlock Avoidance: In this strategy, deadlock can be avoided by examining the state of the system at every step. The distributed system reviews the allocation of resources and wherever it finds an unsafe state, the system backtracks one step and again comes to the safe state. For this, resource allocation takes time whenever requested by a process. Firstly, the system analysis occurs whether the granting of resources will make the system in a safe state or unsafe state then only allocation will be made.
+
+A safe state refers to the state when the system is not in deadlocked state and order is there for the process regarding the granting of requests.
+An unsafe state refers to the state when no safe sequence exists for the system. Safe sequence implies the ordering of a process in such a way that all the processes run to completion in a safe state.
+3. Deadlock Detection and Recovery: In this strategy, deadlock is detected and an attempt is made to resolve the deadlock state of the system. These approaches rely on a Wait-For-Graph (WFG), which is generated and evaluated for cycles in some methods. The following two requirements must be met by a deadlock detection algorithm: 
+
+Progress: In a given period, the algorithm must find all existing deadlocks. There should be no deadlock existing in the system which is undetected under this condition. To put it another way, after all, wait-for dependencies for a deadlock have arisen, the algorithm should not wait for any additional events to detect the deadlock. 
+ No False Deadlocks: Deadlocks that do not exist should not be reported by the algorithm which is called phantom or false deadlocks.
